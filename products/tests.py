7e2e0858c1ec206faa2c9cfac8bda5271101a54c
zspasov <zaprian_spasov@hotmail.com>
2021-08-01 17:32:26 +0300

    adding tests for products app

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+
+
+class ProductModelTest(TestCase):
+
+    def test_one_entry(self):
+        imgpath = 'media/product_img/apple.jpg'
+        entry = Product.objects.create(name="testing product",image=imgpath, description='cool',price=20.3)
+        self.assertEqual(str(entry),'testing product')
+
+
+    def test_with_empty_data(self):
+        with self.assertRaises(Exception):
+            imgpath = 'media/product_img/apple.jpg'
+            entry = Product.objects.create()
+
+    def test_with_exceeded_price(self):
+        with self.assertRaises(Exception):
+            imgpath = 'media/product_img/apple.jpg'
+            entry = Product.objects.create(name="testing product",image=imgpath, description='cool',price=2000000000.3)
+
+
+    def test_with_empty_name(self):
+        with self.assertRaises(Exception):
+            imgpath = 'media/product_img/apple.jpg'
+            entry = Product.objects.create(image=imgpath, description='cool',price=2000000000.3)
