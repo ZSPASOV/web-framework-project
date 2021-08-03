fb8ab8531229a941f1ff2e68dbdf869c7f438b35
zspasov <zaprian_spasov@hotmail.com>
2021-08-03 17:27:26 +0300

    adding tests for favourites app

--- favourites/tests.py
+++ favourites/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import FavouriteProduct
+from django.contrib.auth.models import User
+from products.models import Product
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class FavouriteProductTest(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.user = User.objects.create_user("test_user," "test@gmail.com", "test123")
+        cls.product=Product.objects.create(name="testing product",image='/', description='cool',price=20.3)
+
+    def test_favorite_entry(self):
+        imgpath = 'media/product_img/apple.jpg'
+        entry = FavouriteProduct.objects.create(product=self.product ,user=self.user, is_favourite=False)
+        res = 'product {} {} by {}'.format('testing product', 'marked favourite', '')
+        self.assertEqual(str(entry),res)
